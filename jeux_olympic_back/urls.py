8361f774e8ba337fb63d9d81040ba3a1c37ad2e2
Jadla Ennih <Eljadla.Ennih@outlook.fr>
2024-09-24 20:50:51 +0200

    ajout des url pour le schéma openAPI et Swagger UI

--- jeux_olympic_back/urls.py
+++ jeux_olympic_back/urls.py
@@ -1,29 +1,22 @@
-"""
-URL configuration for jeux_olympic_back project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from events.api_events_docs import CustomSpectacularAPIView
+from drf_spectacular.views import  SpectacularSwaggerView
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
     path('api/users/', include('users.urls')),
     path('api/', include('tickets.urls')),
+
+    # Ajoute l'URL pour le schéma OpenAPI (nécessaire pour Swagger UI)
+     path('api/schema/', CustomSpectacularAPIView.as_view(), name='schema'),
+    # URL pour Swagger UI
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
